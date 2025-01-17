28a0e191eb6be02824744f5f282b8e0e3f9a148b
buharisahith <buharisahith@gmail.com>
2025-01-17 15:26:44 +0530

    port change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // Routes
 app.use('/api/candidates', require('./routes/candidates'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9099;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
